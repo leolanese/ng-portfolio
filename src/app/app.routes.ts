c2e88e3b3d731a68ed962e49c8795bb49b7a19eb
Leo Lanese <laneseleo@gmail.com>
2025-08-13 14:00:54 +0100

    udpated landing h view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,20 @@
 import { Routes } from '@angular/router';
-import { ParentPureSignalComponent } from './child-pure-signal/parent-pure-signal.component';
-import { ExperienceParentComponent } from './experience-pure-signal/experience-parent.component';
-import { WorkshopParentComponent } from './workshop-pure-signal/workshop-parent.component';
 
 export const routes: Routes = [
   {
     path: 'home',
-    component: ParentPureSignalComponent
+    loadComponent: () => import('./child-pure-signal/parent-pure-signal.component').then(m => m.ParentPureSignalComponent)
   },
   {
     path: 'experience',
-    component: ExperienceParentComponent
+    loadComponent: () => import('./experience-pure-signal/experience-parent.component').then(m => m.ExperienceParentComponent)
   },
   {
     path: 'workshops',
-    component: WorkshopParentComponent
+    loadComponent: () => import('./workshop-pure-signal/workshop-parent.component').then(m => m.WorkshopParentComponent)
   },
   {
     path: '',
-    redirectTo: '/experience',
-    pathMatch: 'full'
+    loadComponent: () => import('./child-pure-signal/parent-pure-signal.component').then(m => m.ParentPureSignalComponent)
   }
 ];
